c5b35ba3b5e00748c94c3767ab014a495ad0d3ab
GriffinHarris <griffinjharris1@gmail.com>
2019-08-13 11:03:51 -0600

    modified

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const prettyPrice = (original, pretty) => {
     if (pretty > 1){
         floor = Math.floor(original) + (pretty / 100)
-        console.log(floor)
+        return(floor)
     }else {
         floor = Math.floor(original) + (pretty)
-        console.log(floor)
+        return(floor)
     }
 }
 
